0680b9d6868caf7113374ac6eed59872ad5ee725
Tobiasz <tobiasz.malecki@appunite.com>
2017-06-30 21:25:48 +0200

    assert_called/3

--- lib/mockery/assertions.ex
+++ lib/mockery/assertions.ex
@@ -6,10 +6,30 @@ defmodule Mockery.Assertions do
   def assert_called(mod, fun),
     do: ExUnit.Assertions.assert called?(mod, fun), "#{mod}.#{fun} was not called"
 
+  defmacro assert_called(mod, fun, args) do
+    quote do
+      ExUnit.Assertions.assert unquote(called_with?(mod, fun, args)), unquote(message(mod, fun))
+    end
+  end
+
   defp called?(mod, fun), do: Utils.get_calls(mod, fun) != []
   defp called?(mod, fun, arity) do
     mod
     |> Utils.get_calls(fun)
     |> Enum.any?(&match?({^arity, _}, &1))
+  end
+
+  defp called_with?(mod, fun, args) do
+    quote do
+      unquote(mod)
+      |> Utils.get_calls(unquote(fun))
+      |> Enum.any?(&match?({_, unquote(args)}, &1))
+    end
+  end
+
+  defp message(mod, fun) do
+    quote do
+      "#{unquote(mod)}.#{unquote(fun)} was not called with given arguments"
+    end
   end
 end
--- test/mockery/assertions_test.exs
+++ test/mockery/assertions_test.exs
@@ -93,31 +93,91 @@ defmodule Mockery.AssertionsTest do
 
       assert output =~ "4 tests, 0 failure"
     end
+
+    test "failure" do
+      defmodule FunArityFailure do
+        use ExUnit.Case
+
+        alias Mockery.Assertions
+        alias Mockery.Utils
+
+        test "when function was not called" do
+          Assertions.assert_called A, fun: 0
+        end
+
+        test "when function was called but with different_arity" do
+          Utils.push_call(A, :fun, 0, [])
+
+          Assertions.assert_called A, fun: 2
+        end
+      end
+
+      ExUnit.Server.cases_loaded()
+      output = capture_io(fn -> ExUnit.run end)
+
+      assert output =~ "2 tests, 2 failures"
+      assert output =~ "A.fun/0 was not called"
+      assert output =~ "A.fun/2 was not called"
+    end
   end
 
-  test "failure" do
-    defmodule FunArityFailure do
-      use ExUnit.Case
+  describe "assert_called/3" do
+    test "success" do
+      defmodule ArgsPatternSuccess do
+        use ExUnit.Case
+        require Mockery.Assertions
 
-      alias Mockery.Assertions
-      alias Mockery.Utils
+        alias Mockery.Assertions
+        alias Mockery.Utils
 
-      test "when function was not called" do
-        Assertions.assert_called A, fun: 0
+        test "zero arity" do
+          Utils.push_call(A, :fun, 0, [])
+          Utils.push_call(A, :fun, 2, ["a", "b"])
+
+          Assertions.assert_called A, :fun, []
+        end
+
+        test "positive arity" do
+          Utils.push_call(A, :fun, 0, [])
+          Utils.push_call(A, :fun, 2, ["a", "b"])
+
+          Assertions.assert_called A, :fun, ["a", "b"]
+        end
+
+        test "positive arity, pattern with unbound var" do
+          Utils.push_call(A, :fun, 0, [])
+          Utils.push_call(A, :fun, 2, ["a", "b"])
+
+          Assertions.assert_called A, :fun, ["a", _]
+        end
       end
 
-      test "when function was called but with different_arity" do
-        Utils.push_call(A, :fun, 0, [])
+      ExUnit.Server.cases_loaded()
+      output = capture_io(fn -> ExUnit.run end)
 
-        Assertions.assert_called A, fun: 2
-      end
+      assert output =~ "3 tests, 0 failures"
     end
 
-    ExUnit.Server.cases_loaded()
-    output = capture_io(fn -> ExUnit.run end)
+    test "failure" do
+      defmodule ArgsPatternFailure do
+        use ExUnit.Case
+        require Mockery.Assertions
 
-    assert output =~ "2 tests, 2 failures"
-    assert output =~ "A.fun/0 was not called"
-    assert output =~ "A.fun/2 was not called"
+        alias Mockery.Assertions
+        alias Mockery.Utils
+
+        test "when function was not called with given args pattern" do
+          Utils.push_call(A, :fun, 2, ["a", "b"])
+
+          Assertions.assert_called A, :fun, ["a", "c"]
+        end
+      end
+
+      ExUnit.Server.cases_loaded()
+      output = capture_io(fn -> ExUnit.run end)
+
+      assert output =~ "1 test, 1 failure"
+      assert output =~ "A.fun was not called with given arguments"
+    end
   end
 end

99936a8e0d5211789c0684bd63b28031f5175dcf
Tobiasz <tobiasz.malecki@appunite.com>
2017-07-01 00:04:54 +0200

    remove defdelegate, moved mockery.core to mockery

--- lib/mockery.ex
+++ lib/mockery.ex
@@ -1,4 +1,20 @@
 defmodule Mockery do
-  defdelegate of(mod, opts \\ []),   to: Mockery.Core
-  defdelegate mock(mod, fun, value), to: Mockery.Core
+  alias Mockery.Utils
+
+  def of(mod, opts \\ []) do
+    env = opts[:env] || Mix.env
+
+    cond do
+      env != :test ->
+        mod
+      by = Keyword.get(opts, :by) ->
+        {by, :ok}
+      :else ->
+        {Mockery.Proxy, mod}
+    end
+  end
+
+  def mock(mod, fun, value) do
+    Process.put(Utils.dict_mock_key(mod, fun), value)
+  end
 end
--- test/mockery_test.exs
+++ test/mockery_test.exs
@@ -1,28 +1,27 @@
-defmodule Mockery.CoreTest do
+defmodule MockeryTest do
   use ExUnit.Case, async: true
-  alias Mockery.Core
 
   describe "of/2" do
     test "dev env" do
-      assert Core.of(A, env: :dev) == A
-      assert Core.of(A, by: Z, env: :dev) == A
+      assert Mockery.of(A, env: :dev) == A
+      assert Mockery.of(A, by: Z, env: :dev) == A
     end
 
     test "test env" do
-      assert Core.of(A) == {Mockery.Proxy, A}
-      assert Core.of(A, by: Z) == {Z, :ok}
+      assert Mockery.of(A) == {Mockery.Proxy, A}
+      assert Mockery.of(A, by: Z) == {Z, :ok}
     end
   end
 
   describe "mock/3" do
     test "with name" do
-      Core.mock(Dummy, :fun1, "value1")
+      Mockery.mock(Dummy, :fun1, "value1")
 
       assert Process.get({{Mockery, :mock}, {Dummy, :fun1}}) == "value1"
     end
 
     test "with name and arity" do
-      Core.mock(Dummy, [fun1: 0], "value2")
+      Mockery.mock(Dummy, [fun1: 0], "value2")
 
       assert Process.get({{Mockery, :mock}, {Dummy, {:fun1, 0}}}) == "value2"
     end

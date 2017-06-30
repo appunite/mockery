b1a67b8c636227c9ab39c9f79c7abc200ae7143b
Tobiasz <tobiasz.malecki@appunite.com>
2017-06-30 21:38:55 +0200

    coverage

--- mix.exs
+++ mix.exs
@@ -12,6 +12,8 @@ defmodule Mockery.Mixfile do
       elixir: "~> 1.3",
       elixirc_paths: elixirc_paths(Mix.env),
       package: package(),
+      preferred_cli_env: preferred_cli_env(),
+      test_coverage: [tool: ExCoveralls],
       version: @version
     ]
   end
@@ -22,6 +24,7 @@ defmodule Mockery.Mixfile do
 
   defp deps do
     [
+      {:excoveralls, "~> 0.7", only: :test},
       {:ex_doc, "~> 0.13", only: :dev}
     ]
   end
@@ -47,6 +50,14 @@ defmodule Mockery.Mixfile do
       maintainers: ["Tobiasz Małecki"],
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/amatalai/mockery"}
+    ]
+  end
+
+  defp preferred_cli_env do
+    [
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.travis": :test
     ]
   end
 end

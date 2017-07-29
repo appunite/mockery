6dfbbeb19b7dbf0b8b02d36cd5b17fb15db63f0c
Tobiasz <tobiasz.malecki@appunite.com>
2017-07-30 00:33:40 +0200

    refute_called/3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,5 @@
 
 ## 1.0.0
 * Added `use Mockery` for importing both `Mockery` and `Mockery.Assertions`
-* Loosen elixir version requirement to `~> 1.1`
+* Added `refute_called/2` and `refute_called/3`
+* Loosen elixir version requirement from `~> 1.3` to `~> 1.1`
--- test/mockery/assertions_test.exs
+++ test/mockery/assertions_test.exs
@@ -282,6 +282,10 @@ defmodule Mockery.AssertionsTest do
       alias Mockery.Assertions
       alias Mockery.Utils
 
+      test "when function was not called" do
+        Assertions.assert_called A, :fun, ["a", "c"]
+      end
+
       test "when function was not called with given args pattern" do
         Utils.push_call(A, :fun, 2, ["a", "b"])
 
@@ -292,7 +296,70 @@ defmodule Mockery.AssertionsTest do
     load_cases()
     output = capture_io(fn -> ExUnit.run end)
 
-    assert output =~ "1 test, 1 failure"
+    assert output =~ "2 tests, 2 failures"
     assert output =~ "A.fun was not called with given arguments"
+  end
+
+  test "refute_called/3 success" do
+    defmodule ArgsPatternRefuteSuccess do
+      use ExUnit.Case
+      require Mockery.Assertions
+
+      alias Mockery.Assertions
+      alias Mockery.Utils
+
+      test "when function was not called" do
+        Assertions.refute_called A, :fun, ["a", "c"]
+      end
+
+      test "when function was not called with given args pattern" do
+        Utils.push_call(A, :fun, 2, ["a", "b"])
+
+        Assertions.refute_called A, :fun, ["a", "c"]
+      end
+    end
+
+    load_cases()
+    output = capture_io(fn -> ExUnit.run end)
+
+    assert output =~ "2 tests, 0 failures"
+  end
+
+  test "refute_called/3 failure" do
+    defmodule ArgsPatternRefuteFailure do
+      use ExUnit.Case
+      require Mockery.Assertions
+
+      alias Mockery.Assertions
+      alias Mockery.Utils
+
+      test "zero arity" do
+        Utils.push_call(A, :fun, 0, [])
+        Utils.push_call(A, :fun, 2, ["a", "b"])
+
+        Assertions.refute_called A, :fun, []
+      end
+
+      test "positive arity" do
+        Utils.push_call(A, :fun, 0, [])
+        Utils.push_call(A, :fun, 2, ["a", "b"])
+
+        Assertions.refute_called A, :fun, ["a", "b"]
+      end
+
+      test "positive arity, pattern with unbound var" do
+        Utils.push_call(A, :fun, 0, [])
+        Utils.push_call(A, :fun, 2, ["a", "b"])
+
+        Assertions.refute_called A, :fun, ["a", _]
+      end
+    end
+
+    load_cases()
+    output = capture_io(fn -> ExUnit.run end)
+
+    assert output =~ "3 tests, 3 failures"
+    assert output =~ "A.fun was called with given arguments at least once"
+
   end
 end

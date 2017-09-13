bc2020dc5d528ada10c388e69e428db985a77ef0
Tobiasz <tobiasz.malecki@appunite.com>
2017-09-13 13:43:20 +0200

    additional utils tests

--- lib/mockery/utils.ex
+++ lib/mockery/utils.ex
@@ -1,6 +1,11 @@
 defmodule Mockery.Utils do
   @moduledoc false #this module is private to Mockery
 
+  @application Mockery.of(Application)
+  def history_enabled? do
+    Process.get(Mockery.History, @application.get_env(Mockery, :history, false))
+  end
+
   # removes unnecessary `Elixir.` prefix from module names
   def print_mod(mod), do: mod |> to_string |> remove_elixir_prefix()
 
@@ -14,10 +19,6 @@ defmodule Mockery.Utils do
     key = dict_called_key(mod, fun)
 
     Process.put(key, [{arity, args} | get_calls(mod, fun)])
-  end
-
-  def history_enabled? do
-    Process.get(Mockery.History, Application.get_env(Mockery, :history, false))
   end
 
   defp remove_elixir_prefix("Elixir." <> rest), do: rest
--- test/mockery/utils_test.exs
+++ test/mockery/utils_test.exs
@@ -1,5 +1,7 @@
 defmodule Mockery.UtilsTest do
   use ExUnit.Case, async: true
+  use Mockery
+
   alias Mockery.Utils
 
   test "print_mod/1 for elixir module" do
@@ -8,5 +10,24 @@ defmodule Mockery.UtilsTest do
 
   test "print_mod/2 for erlang module" do
     assert Utils.print_mod(:crypto) == ":crypto"
+  end
+
+  test "history_enabled?/0 returns false by default" do
+    refute Utils.history_enabled?
+  end
+
+  test "history_enabled?/0 changed by global config" do
+    mock Application, :get_env, true
+
+    assert Utils.history_enabled?
+    assert_called Application, :get_env, [Mockery, :history, _], 1
+  end
+
+  test "history_enabled?/0 global config ignored when process config present" do
+    mock Application, :get_env, true
+    enable_history(false)
+
+    refute Utils.history_enabled?
+    assert_called Application, :get_env, [Mockery, :history, _], 1
   end
 end

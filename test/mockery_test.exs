f30c8ac952cc3877fa0624a7194b2cf70875f988
Tobiasz <tobiasz.malecki@appunite.com>
2017-09-15 00:22:05 +0200

    of/2 support for elixir modules in string format

--- test/mockery_test.exs
+++ test/mockery_test.exs
@@ -1,14 +1,40 @@
 defmodule MockeryTest do
   use ExUnit.Case, async: true
 
-  test "of/2 dev env" do
-    assert Mockery.of(A, env: :dev) == A
-    assert Mockery.of(A, by: Z, env: :dev) == A
+  test "of/2 dev env (atom erlang mod)" do
+    assert Mockery.of(:a, env: :dev) == :a
+    assert Mockery.of(:a, by: Z, env: :dev) == :a
+    assert Mockery.of(:a, by: "Z", env: :dev) == :a
   end
 
-  test "of/2 test env" do
+  test "of/2 test env (atom erlang mod)" do
+    assert Mockery.of(:a) == {Mockery.Proxy, :a}
+    assert Mockery.of(:a, by: Z) == {Z, :ok}
+    assert Mockery.of(:a, by: "Z") == {Z, :ok}
+  end
+
+  test "of/2 dev env (atom elixir mod)" do
+    assert Mockery.of(A, env: :dev) == A
+    assert Mockery.of(A, by: Z, env: :dev) == A
+    assert Mockery.of(A, by: "Z", env: :dev) == A
+  end
+
+  test "of/2 test env (atom elixir mod)" do
     assert Mockery.of(A) == {Mockery.Proxy, A}
     assert Mockery.of(A, by: Z) == {Z, :ok}
+    assert Mockery.of(A, by: "Z") == {Z, :ok}
+  end
+
+  test "of/2 dev env (string elixir mod)" do
+    assert Mockery.of("A", env: :dev) == A
+    assert Mockery.of("A", by: Z, env: :dev) == A
+    assert Mockery.of("A", by: "Z", env: :dev) == A
+  end
+
+  test "of/2 test env (string elixir mod)" do
+    assert Mockery.of("A") == {Mockery.Proxy, A}
+    assert Mockery.of("A", by: Z) == {Z, :ok}
+    assert Mockery.of("A", by: "Z") == {Z, :ok}
   end
 
   test "mock/3 with name" do

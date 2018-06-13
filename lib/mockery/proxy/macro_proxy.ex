f0a17921f201408204fbd0497cdbb83bc5cd4ea3
Tobiasz <tobiasz.malecki@appunite.com>
2018-06-13 20:19:20 +0200

    resolves credo suggestion

--- lib/mockery/proxy/macro_proxy.ex
+++ lib/mockery/proxy/macro_proxy.ex
@@ -1,10 +1,12 @@
 defmodule Mockery.Proxy.MacroProxy do
   @moduledoc false
 
+  alias Mockery.Proxy
+
   def unquote(:"$handle_undefined_function")(name, args) do
     {mod, by} = Process.get(Mockery.MockableModule)
     Process.delete(Mockery.MockableModule)
 
-    Mockery.Proxy.do_proxy(mod, name, args, by)
+    Proxy.do_proxy(mod, name, args, by)
   end
 end

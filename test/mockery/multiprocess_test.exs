89b30372f683de16908e819f6ea7d1d5ce0ef808
Roman Heinrich <roman.heinrich@gmail.com>
2018-08-21 15:34:48 +0200

    code auto-formatted

--- test/mockery/multiprocess_test.exs
+++ test/mockery/multiprocess_test.exs
@@ -10,6 +10,7 @@ defmodule Mockery.MultiprocessTest do
 
   defmodule B do
     import Mockery.Macro
+
     def run do
       mockable(A).run
     end
@@ -17,14 +18,14 @@ defmodule Mockery.MultiprocessTest do
 
   test "same process mocking works" do
     mock(A, [run: 0], "mock")
-    assert B.run == "mock"
+    assert B.run() == "mock"
   end
 
   test "multiprocess mocking does not work" do
     mock(A, [run: 0], "mock")
 
-    spawn fn ->
-      assert B.run == "real"
-    end
+    spawn(fn ->
+      assert B.run() == "real"
+    end)
   end
 end

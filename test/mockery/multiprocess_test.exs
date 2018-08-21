5d7a6405238639c8a6ae42f1cc67cd8a5475e45b
Roman Heinrich <roman.heinrich@gmail.com>
2018-08-21 11:29:25 +0200

    test to show that mockery does not work across processes
    problem: 
    - there is not documentation that explains why mockery is not keeping mocks intact across multiple erlang processes
    
    
    solution: 
    - we start with a test to make this behaviour explicit

---
+++ test/mockery/multiprocess_test.exs
@@ -0,0 +1,30 @@
+defmodule Mockery.MultiprocessTest do
+  use ExUnit.Case, async: true
+  import Mockery
+
+  defmodule A do
+    def run do
+      "real"
+    end
+  end
+
+  defmodule B do
+    import Mockery.Macro
+    def run do
+      mockable(A).run
+    end
+  end
+
+  test "same process mocking works" do
+    mock(A, [run: 0], "mock")
+    assert B.run == "mock"
+  end
+
+  test "multiprocess mocking does not work" do
+    mock(A, [run: 0], "mock")
+
+    spawn fn ->
+      assert B.run == "real"
+    end
+  end
+end

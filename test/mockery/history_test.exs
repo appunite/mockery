12f1a6b19a08a6ffa9dfbfc789b7c58a7b129888
Tobiasz <tobiasz.malecki@appunite.com>
2019-10-05 22:48:57 +0200

    Remove usage of ExUnit private api from Mockery.History tests

--- test/mockery/history_test.exs
+++ test/mockery/history_test.exs
@@ -3,118 +3,58 @@ defmodule Mockery.HistoryTest do
   use ExUnit.Case
   use Mockery
 
-  import ExUnit.CaptureIO
   import IO.ANSI
 
-  defp load_cases do
-    if {:cases_loaded, 0} in ExUnit.Server.__info__(:functions) do
-      ExUnit.Server.cases_loaded()
-    end
+  test "failure with too few args is marked in red" do
+    enable_history()
+    Mockery.Utils.push_call(A, :fun, 1, ["a"])
 
-    if {:modules_loaded, 0} in ExUnit.Server.__info__(:functions) do
-      ExUnit.Server.modules_loaded()
-    end
+    tested = fn -> Mockery.Assertions.assert_called(A, :fun, ["a", "b"]) end
+    %{message: message} = assert_raise(ExUnit.AssertionError, tested)
+
+    assert message =~ ~s(#{red()}["a"]#{white()})
   end
 
-  test "failure with too few args" do
-    defmodule Mod1 do
-      use ExUnit.Case
-      use Mockery
+  test "failure with too many args is marked in red" do
+    enable_history()
+    Mockery.Utils.push_call(A, :fun, 3, ["a", "b", "c"])
 
-      test "failure" do
-        Mockery.Utils.push_call(A, :fun, 1, ["a"])
+    tested = fn -> Mockery.Assertions.assert_called(A, :fun, ["a", "b"]) end
+    %{message: message} = assert_raise(ExUnit.AssertionError, tested)
 
-        enable_history()
-        Mockery.Assertions.assert_called(A, :fun, ["a", "b"])
-      end
-    end
-
-    load_cases()
-    output = capture_io(fn -> ExUnit.run() end)
-
-    assert output =~ "1 test, 1 failure"
-    assert output =~ ~s(#{red()}["a"]#{white()})
+    assert message =~ ~s(#{red()}["a", "b", "c"]#{white()})
   end
 
-  test "failure with too many args" do
-    defmodule Mod2 do
-      use ExUnit.Case
-      use Mockery
+  test "failure with correct number of args marks matched args in green and unmatched in red" do
+    enable_history()
+    Mockery.Utils.push_call(A, :fun, 2, ["a", "c"])
 
-      test "failure" do
-        Mockery.Utils.push_call(A, :fun, 3, ["a", "b", "c"])
+    tested = fn -> Mockery.Assertions.assert_called(A, :fun, ["a", "b"]) end
+    %{message: message} = assert_raise(ExUnit.AssertionError, tested)
 
-        enable_history()
-        Mockery.Assertions.assert_called(A, :fun, ["a", "b"])
-      end
-    end
-
-    load_cases()
-    output = capture_io(fn -> ExUnit.run() end)
-
-    assert output =~ "1 test, 1 failure"
-    assert output =~ ~s(#{red()}["a", "b", "c"]#{white()})
+    assert message =~ ~s(#{white()}[#{green()}"a"#{white()}, #{red()}"c"#{white()}])
   end
 
-  test "failure inside pattern" do
-    defmodule Mod3 do
-      use ExUnit.Case
-      use Mockery
+  test "failure with correct number of args marks unbound args in green as they always match" do
+    enable_history()
+    Mockery.Utils.push_call(A, :fun, 2, ["a", "c"])
 
-      test "failure" do
-        Mockery.Utils.push_call(A, :fun, 2, ["a", "c"])
+    tested = fn -> Mockery.Assertions.assert_called(A, :fun, [_, "b"]) end
+    %{message: message} = assert_raise(ExUnit.AssertionError, tested)
 
-        enable_history()
-        Mockery.Assertions.assert_called(A, :fun, ["a", "b"])
-      end
-    end
-
-    load_cases()
-    output = capture_io(fn -> ExUnit.run() end)
-
-    assert output =~ "1 test, 1 failure"
-    assert output =~ ~s(#{white()}[#{green()}"a"#{white()}, #{red()}"c"#{white()}])
+    assert message =~ ~s(#{white()}[#{green()}"a"#{white()}, #{red()}"c"#{white()}])
   end
 
-  test "failure inside pattern with unbound var" do
-    defmodule Mod4 do
-      use ExUnit.Case
-      use Mockery
+  test "failure with correct number of args marks pinned args according to their correctness" do
+    enable_history()
+    Mockery.Utils.push_call(A, :fun, 2, ["a", "c"])
 
-      test "failure" do
-        Mockery.Utils.push_call(A, :fun, 2, ["a", "c"])
+    arg1 = "a"
+    arg2 = "b"
+    tested = fn -> Mockery.Assertions.assert_called(A, :fun, [^arg1, ^arg2]) end
+    %{message: message} = assert_raise(ExUnit.AssertionError, tested)
 
-        enable_history()
-        Mockery.Assertions.assert_called(A, :fun, [_, "b"])
-      end
-    end
-
-    load_cases()
-    output = capture_io(fn -> ExUnit.run() end)
-
-    assert output =~ "1 test, 1 failure"
-    assert output =~ ~s(#{white()}[#{green()}"a"#{white()}, #{red()}"c"#{white()}])
-  end
-
-  test "failure inside pattern with pin" do
-    defmodule Mod5 do
-      use ExUnit.Case
-      use Mockery
-
-      test "failure" do
-        Mockery.Utils.push_call(A, :fun, 2, ["a", "c"])
-
-        enable_history()
-        x = "b"
-        Mockery.Assertions.assert_called(A, :fun, ["a", ^x])
-      end
-    end
-
-    load_cases()
-    output = capture_io(fn -> ExUnit.run() end)
-
-    assert output =~ "1 test, 1 failure"
-    assert output =~ ~s(#{white()}[#{green()}"a"#{white()}, #{red()}"c"#{white()}])
+    assert message =~ ~s(#{white()}[#{green()}"a"#{white()}, #{red()}"c"#{white()}])
   end
 
   test "enable_history/0" do

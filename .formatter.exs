e19b9206ac5bae2c8c9f550041958af6c3597fc4
Tobiasz Małecki <tobiasz.malecki@appunite.com>
2025-09-08 02:42:19 +0200

    Improve .formatter.exs config

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,7 @@
 locals_without_parens = [defmock: 2, defmock: 3]
 
 [
-  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["{mix,.formatter,.credo}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: locals_without_parens,
   export: [
     locals_without_parens: locals_without_parens

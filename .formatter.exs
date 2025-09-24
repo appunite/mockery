30b95b59cff69e09f8115b22d7088d46679d55a3
Tobiasz Małecki <tobiasz.malecki@appunite.com>
2025-09-24 17:26:30 +0200

    Drop removed macros from .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,12 +1,6 @@
 locals_without_parens = [
   defmock: 2,
   defmock: 3,
-  assert_called: 2,
-  assert_called: 3,
-  assert_called: 4,
-  refute_called: 2,
-  refute_called: 3,
-  refute_called: 4,
   assert_called!: 2,
   assert_called!: 3,
   refute_called!: 2,
